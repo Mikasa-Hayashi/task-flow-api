836efbcab89c5c7a6720cf80929690da99e14be3
Ivan Maschenko <ivandenisovich2005@icloud.com>
2026-06-13 19:18:25 +0300

    chore: initialize models module

---
+++ app/task_flow_api/models/__init__.py
@@ -0,0 +1,6 @@
+from task_flow_api.models.comment import Comment
+from task_flow_api.models.project import Project
+from task_flow_api.models.project_member import ProjectMember
+from task_flow_api.models.task import Task
+
+__all__ = ["Comment", "Project", "ProjectMember", "Task"]
